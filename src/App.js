ad3480ffbbb622d12f651cb5ea7e299952c0b432
Sebe97 <31695491+Sebe97@users.noreply.github.com>
2022-03-01 21:22:41 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HamburgerNavbar from './Menu/HamburgerNavbar';
+import HomePage from './Componente/HomePage';
+
+function App() {
+  return (
+    <>
+    {/* <HomePage/> */}
+    <HamburgerNavbar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Menu/HamburgerNavbar.jsx
@@ -0,0 +1,22 @@
+import { slide as Menu } from 'react-burger-menu';
+import React from 'react'
+import './Sidebar.css';
+
+export default function HamburgerNavbar(bbb) {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href="/salads">
+        Salads
+      </a>
+      <a className="menu-item" href="/pizzas">
+        Pizzas
+      </a>
+      <a className="menu-item" href="/desserts">
+        Desserts
+      </a>
+    </Menu>
+  );
+};
